2377ba1b6dc5e7ad05c5d4b01be214f3a5cced8f
hanamichizz <30983981+hanamichizz@users.noreply.github.com>
2018-04-29 20:46:17 +0700

    Update

--- app/src/main/java/com/example/hanamichiz/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/hanamichiz/projectandroid/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.hanamichiz.projectandroid;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> myAdepter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdepter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdepter);
     }
 }
