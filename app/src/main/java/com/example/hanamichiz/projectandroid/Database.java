17cc8540da3372746b681b0faa714b1a4b9327a8
hanamichizz <gukanko@gmail.com>
2018-05-02 01:27:16 +0700

    Database

---
+++ app/src/main/java/com/example/hanamichiz/projectandroid/Database.java
@@ -0,0 +1,10 @@
+package com.example.hanamichiz.projectandroid;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Database {
+    static  FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public  Database() {
+
+    }
+}
--- app/src/main/java/com/example/hanamichiz/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/hanamichiz/projectandroid/MainActivity.java
@@ -1,11 +1,25 @@
 package com.example.hanamichiz.projectandroid;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,4 +36,28 @@ public class MainActivity extends AppCompatActivity {
         myAdepter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdepter);
     }
+    public void OnClickTest(View view) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        EditText foodtype = (EditText) findViewById(R.id.editText);
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        Task<DocumentReference> documentReferenceTask = db.collection("foodType")
+                .add(foodtype.getText())
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
 }
